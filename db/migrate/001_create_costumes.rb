18a5e0ae6412cdcbf2dac252561904fb7bbd18a1
Donna Amos <donnacamos.developer@gmail.com>
2019-06-22 00:50:09 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -6,9 +6,8 @@ class CreateCostumes < ActiveRecord::Migrantion[4.2]
       t.integer :price 
       t.integer :size 
       t.img :image_url 
+      t.timestamps
     end 
-    created_at 
-    updated_at 
   end 
 end 
 # By convention, the class name should match the part of the
