223ea975245ede77fbd3ac301f35439b535516ca
Donna Amos <donnacamos.developer@gmail.com>
2019-06-22 00:43:34 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,13 @@
 # Create your haunted_houses migration here
+class HauntedHouses < ActiveRecord::Migration[4.2]
+  def change 
+    create_table :costume_stores do |t| 
+    t.string :name 
+    t.string :location
+    t.integer :costume_inventory 
+    t.integer :employee_count 
+    t.string :business_status 
+    t.integer :opening_time
+    t.integer :closing_time
+  end 
+end 
