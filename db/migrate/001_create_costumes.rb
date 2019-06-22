995b116ede07b67bbdcd6827dafd7186b8003477
Donna Amos <donnacamos.developer@gmail.com>
2019-06-22 01:10:24 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -5,7 +5,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
       t.string :name 
       t.integer :price 
       t.integer :size 
-      t.img :image_url 
+      t.string :image_url 
       t.timestamps
     end 
   end 
