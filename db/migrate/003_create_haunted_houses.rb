c87b0daeac215d95f88d86ab3747f106743645e1
Donna Amos <donnacamos.developer@gmail.com>
2019-06-22 01:16:31 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -6,9 +6,9 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
     t.string :location
     t.string :theme 
     t.float :price
-    t.string :family_friendly_status 
+    t.boolean :family_friendly 
     t.datetime :opening_date
-    t.integer :closing_date
+    t.datetime :closing_date
     t.string :long_description
   end 
 end 
