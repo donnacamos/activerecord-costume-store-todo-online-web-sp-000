f94a35755779b9e2b1f4d551a1814a2813f216f2
Donna Amos <donnacamos.developer@gmail.com>
2019-06-22 01:24:11 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "location"
     t.integer "costume_inventory"
     t.integer "num_of_employees"
-    t.boolean "still_in_business"
+    t.boolean "business_status"
     t.datetime "opening_time"
     t.datetime "closing_time"
   end
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 3) do
     t.boolean "family_friendly"
     t.datetime "opening_date"
     t.datetime "closing_date"
-    t.text "description"
+    t.text "long_description"
   end
 
 end
